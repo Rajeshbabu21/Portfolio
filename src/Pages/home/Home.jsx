c0888c17b9551e3750854a5ad4f393df0506ec12
Rajeshbabu21 <rajeshgit2004@gmail.com>
2024-09-28 22:19:40 +0530

    Completeed the navbar

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+// import Profile from "../../assets/home.jpg" 
+// import Profile from '../../assets/home.jpg'
+import Profile from "../../assets/home.jpg"
+
+import {Link} from "react-router-dom"
+import { FaArrowRight } from 'react-icons/fa'
+import "./home.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='home grid section'>
+      <img src={Profile} alt='' className='home__img' />
+
+      <div className='home__content'>
+        <div className='home__data'>
+          <h1 className='home__title'>
+            <span>I'm Rajeshbabu.</span>
+            Front-End Developer & DSA Enthusiast
+          </h1>
+
+          <p className='home__description'>
+            Hey there! I'm Rajeshbabu, a Front-End Developer and DSA Enthusiast.
+            I build clean, responsive websites with HTML, CSS, React.js, and
+            Tailwind CSS. With a passion for problem-solving through Data
+            Structures and Algorithms in JAVA, I aim to create impactful software. Let’s
+            create something awesome together!
+          </p>
+
+          <Link to="/about" className='button'>
+          More About Me {' '}
+          <span className='button__icon'>
+            <FaArrowRight />
+          </span>
+          </Link>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { links } from '../data'
+import { NavLink } from 'react-router-dom'
+import "./navbar.css"
 
 const Navbar = () => {
+ const [showMenu,setShowMenu ] = useState(false)
   return (
-    <div></div>
+    <nav className='nav'>
+     <div className={`${showMenu ? 'nav__menu show-menu'  :"nav__menu" }`}>
+      <ul className='nav__list'>
+       {links.map(({name,icon,path},index)=>{
+        return(
+         <li className="nav__item key = {item}">
+          <NavLink to = {path}  className={({isActive})=>{
+           return isActive ? 'nav__link active-nav' : 'nav__link'
+          }}
+          onClick={()=>{
+           setShowMenu(!showMenu)
+          }}
+          
+          
+          >
+           {icon}
+           <h3 className="nav__name">{name}</h3>
+           </NavLink>
+         </li>
+        )
+       })}
+
+      </ul>
+     </div>
+
+     <div className={`${showMenu ? "nav__toggle animate-toggle":'nav__toggle'}`} onClick={()=>{
+      setShowMenu(!showMenu)
+     }}>
+      <span></span>
+      <span></span>
+      <span></span>
+     </div>
+    </nav>
   )
 }
 
