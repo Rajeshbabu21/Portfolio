195f8f836a0ee229bf06cc61bc8c77c40483c727
Rajeshbabu21 <rajeshgit2004@gmail.com>
2024-09-30 11:57:40 +0530

    resize the image

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 // import Profile from "../../assets/home.jpg" 
 // import Profile from '../../assets/home.jpg'
-import Profile from "../../assets/home1.jpeg"
+import Profile from "../../assets/home1.jpg"
 
 import {Link} from "react-router-dom"
 import { FaArrowRight } from 'react-icons/fa'
